f1a127add3f68e215314a4e67fee59fe9125a5b0
Ayush Kumar Jha <akjha498@gmail.com>
2021-12-15 21:08:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,147 @@
+function updateMap() {
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(rsp => {
+            console.log(rsp.data)//data will come from data.json
+            rsp.data.forEach(element => {
+                latitude = element.latitude;
+                longitude = element.longitude;
+                var color;
+                cases = element.infected;
+                if(cases>255){
+                    color=`rgb(255, 0, 0)`
+                }
+                else{
+                    color=`rgb(${cases}, 0, 0)`
+                }
+                //Mark On The Map;
+                 new mapboxgl.Marker({
+                    draggable: true,
+                    color:color
+                    })
+                
+                    .setLngLat([longitude, latitude])
+
+                    .addTo(map);
+
+                    map.on('mousemove', 'state-fills', (e) => {
+                        if (e.features.length > 0) {
+                        if (hoveredStateId !== null) {
+                        map.setFeatureState(
+                        { source: 'states', id: hoveredStateId },
+                        { hover: false }
+                        );
+                        }
+                        hoveredStateId = e.features[0].id;
+                        map.setFeatureState(
+                        { source: 'states', id: hoveredStateId },
+                        { hover: true }
+                        );
+                        }
+                        });
+            });
+        })
+
+}
+
+const size = 200;
+
+// This implements `StyleImageInterface`
+// to draw a pulsing dot icon on the map.
+const pulsingDot = {
+    width: size,
+    height: size,
+    data: new Uint8Array(size * size * 4),
+
+    // When the layer is added to the map,
+    // get the rendering context for the map canvas.
+    onAdd: function () {
+        const canvas = document.createElement('canvas');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        this.context = canvas.getContext('2d');
+    },
+
+    // Call once before every frame where the icon will be used.
+    render: function () {
+        const duration = 1000;
+        const t = (performance.now() % duration) / duration;
+
+        const radius = (size / 2) * 0.3;
+        const outerRadius = (size / 2) * 0.7 * t + radius;
+        const context = this.context;
+
+        // Draw the outer circle.
+        context.clearRect(0, 0, this.width, this.height);
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            outerRadius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = `rgba(255, 200, 200, ${1 - t})`;
+        context.fill();
+
+        // Draw the inner circle.
+        context.beginPath();
+        context.arc(
+            this.width / 2,
+            this.height / 2,
+            radius,
+            0,
+            Math.PI * 2
+        );
+        context.fillStyle = 'rgba(255, 100, 100, 1)';
+        context.strokeStyle = 'white';
+        context.lineWidth = 2 + 4 * (1 - t);
+        context.fill();
+        context.stroke();
+
+        // Update this image's data with data from the canvas.
+        this.data = context.getImageData(
+            0,
+            0,
+            this.width,
+            this.height
+        ).data;
+
+        // Continuously repaint the map, resulting
+        // in the smooth animation of the dot.
+        map.triggerRepaint();
+
+        // Return `true` to let the map know that the image was updated.
+        return true;
+    }
+};
+
+map.on('load', () => {
+    map.addImage('pulsing-dot', pulsingDot, { pixelRatio: 2 });
+
+    map.addSource('dot-point', {
+        'type': 'geojson',
+        'data': {
+            'type': 'FeatureCollection',
+            'features': [
+                {
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [10, 10] // icon position [lng, lat]
+                    }
+                }
+            ]
+        }
+    });
+    map.addLayer({
+        'id': 'layer-with-pulsing-dot',
+        'type': 'symbol',
+        'source': 'dot-point',
+        'layout': {
+            'icon-image': 'pulsing-dot'
+        }
+    });
+});
+let interval=2000
+updateMap();
