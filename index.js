1809b6cbe8b2988d02b6b4c13f710e461b8a3605
Ayush2420 <86476886+Ayush2420@users.noreply.github.com>
2021-12-15 21:19:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,22 +23,7 @@ function updateMap() {
                     .setLngLat([longitude, latitude])
 
                     .addTo(map);
-
-                    map.on('mousemove', 'state-fills', (e) => {
-                        if (e.features.length > 0) {
-                        if (hoveredStateId !== null) {
-                        map.setFeatureState(
-                        { source: 'states', id: hoveredStateId },
-                        { hover: false }
-                        );
-                        }
-                        hoveredStateId = e.features[0].id;
-                        map.setFeatureState(
-                        { source: 'states', id: hoveredStateId },
-                        { hover: true }
-                        );
-                        }
-                        });
+ 
             });
         })
 
